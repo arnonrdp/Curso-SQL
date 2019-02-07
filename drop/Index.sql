6906a3e21da12202e45579db04de37743bc8d54b
Arnon <arnonrdp@gmail.com>
2019-02-07 11:20:00 -0200

    Remoção de Index e View

---
+++ drop/Index.sql
@@ -0,0 +1,3 @@
+-- O comando DROP INDEX é usado para excluir um índice em uma tabela. 
+
+DROP INDEX idx_firstname;
---
+++ drop/View.sql
@@ -0,0 +1,4 @@
+-- O comando DROP VIEW exclui uma vista. 
+
+# O SQL a seguir descarta a visualização "Persons from Rio ":
+DROP VIEW [Persons from Rio];
