ea835e844f2dbbdd874dd6c714f9648b870cd179
Arnon <arnonrdp@gmail.com>
2019-03-27 11:20:00 -0300

    Removida pasta Create

--- create/Column.sql
+++
@@ -1,7 +0,0 @@
--- O comando ADD é usado para adicionar uma coluna em uma tabela existente.
-
-# O SQL a seguir adiciona uma coluna "Email" à tabela "Persons":
-ALTER TABLE
-    Persons
-ADD
-    Email VARCHAR(50);
--- create/Database.sql
+++
@@ -1,4 +0,0 @@
--- O comando CREATE DATABASE é usado para criar um novo banco de dados SQL.
-
-# O SQL a seguir cria um banco de dados chamado "testDB":
-CREATE DATABASE sampleDB;
